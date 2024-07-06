79b7efa6dda85b1ef097345fb85ea9fb725a2347
Marcel Stemmeler <57675018+heini208@users.noreply.github.com>
2024-07-07 00:47:10 +0200

    Remove files and directories as per .gitignore

--- build/safety_controller/prefix_override/sitecustomize.py
+++
@@ -1,3 +0,0 @@
-import sys
-sys.real_prefix = sys.prefix
-sys.prefix = sys.exec_prefix = '/home/marcel/ros2_ws/src/safety_controller/install/safety_controller'
--- build/safety_controller/share/safety_controller/hook/pythonpath_develop.sh
+++
@@ -1,3 +0,0 @@
-# generated from colcon_core/shell/template/hook_prepend_value.sh.em
-
-_colcon_prepend_unique_value PYTHONPATH "/home/marcel/ros2_ws/src/safety_controller/build/safety_controller"
